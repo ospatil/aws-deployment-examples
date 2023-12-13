b88e430685f35137d59a7bfc3a1c8c83da39a0ca
Omkar Patil <ospatil@gmail.com>
2023-12-13 07:50:00 -0500

    Add api origin and restructure code

--- packages/ec2/lib/ec2-stack.ts
+++ packages/ec2/lib/ec2-stack.ts
@@ -1,13 +1,13 @@
 import * as cdk from 'aws-cdk-lib'
 import * as asg from 'aws-cdk-lib/aws-autoscaling'
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront'
-import * as s3Origins from 'aws-cdk-lib/aws-cloudfront-origins'
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins'
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
 import * as ec2 from 'aws-cdk-lib/aws-ec2'
 import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2'
 import * as iam from 'aws-cdk-lib/aws-iam'
 import * as s3 from 'aws-cdk-lib/aws-s3'
-import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment'
+import * as s3Deploy from 'aws-cdk-lib/aws-s3-deployment'
 import { Construct } from 'constructs'
 import { DynamoDBInsertResource, PrefixListGetResource } from 'custom-resources'
 import * as execa from 'execa'
@@ -31,30 +31,12 @@ export class Ec2Stack extends cdk.Stack {
     const vpc = this.createVpc()
 
     // create SG for LB
-    const albSg = new ec2.SecurityGroup(this, 'aws-examples-lb-sg', {
-      vpc,
-      securityGroupName: 'aws-examples-lb-sg',
-      description: 'Security group for ALB',
-    })
-    albSg.addIngressRule(
-      ec2.Peer.prefixList(prefixList.prefixListId),
-      ec2.Port.tcp(443),
-      'Allow access from cloudfront',
-    )
+    const albSg = this.createAlbSg(vpc, prefixList)
 
     // create SG for ASG
-    const asgSg = new ec2.SecurityGroup(this, 'aws-examples-asg-sg', {
-      vpc,
-      securityGroupName: 'aws-examples-asg-sg',
-      description: 'Security group for ASG',
-    })
-    asgSg.addIngressRule(
-      ec2.Peer.securityGroupId(albSg.securityGroupId),
-      ec2.Port.tcp(3000),
-      'Allow access from ALB',
-    )
+    const asgSg = this.createAsgSg(vpc, albSg)
 
-    this.createInstanceConnectEp(vpc, asgSg)
+    const instanceConnectEp = this.createInstanceConnectEp(vpc, asgSg)
 
     // create launch template
     const launchTemplate = this.createLaunchTemplate(asgSg)
@@ -69,8 +51,8 @@ export class Ec2Stack extends cdk.Stack {
     const distribution = this.createCloudFrontDistribution(s3Bucket, lb)
   }
 
-  createVpc() {
-    return new ec2.Vpc(this, 'aws-examples-vpc', {
+  private createVpc() {
+    return new ec2.Vpc(this, 'vpc', {
       vpcName: 'aws-examples-vpc',
       ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
       maxAzs: 2,
@@ -96,7 +78,35 @@ export class Ec2Stack extends cdk.Stack {
     })
   }
 
-  createDynamodb() {
+  private createAlbSg(vpc: ec2.Vpc, prefixList: PrefixListGetResource) {
+    const albSg = new ec2.SecurityGroup(this, 'lb-sg', {
+      vpc,
+      securityGroupName: 'aws-examples-lb-sg',
+      description: 'Security group for ALB',
+    })
+    albSg.addIngressRule(
+      ec2.Peer.prefixList(prefixList.prefixListId),
+      ec2.Port.tcp(443),
+      'Allow access from cloudfront',
+    )
+    return albSg
+  }
+
+  private createAsgSg(vpc: cdk.aws_ec2.Vpc, albSg: cdk.aws_ec2.SecurityGroup) {
+    const asgSg = new ec2.SecurityGroup(this, 'asg-sg', {
+      vpc,
+      securityGroupName: 'aws-examples-asg-sg',
+      description: 'Security group for ASG',
+    })
+    asgSg.addIngressRule(
+      ec2.Peer.securityGroupId(albSg.securityGroupId),
+      ec2.Port.tcp(3000),
+      'Allow access from ALB',
+    )
+    return asgSg
+  }
+
+  private createDynamodb() {
     return new dynamodb.Table(this, this.dynamodbTableName, {
       tableName: this.dynamodbTableName,
       partitionKey: {
@@ -108,19 +118,19 @@ export class Ec2Stack extends cdk.Stack {
     })
   }
 
-  addDynamoDBRecord(tableName: string, tableArn: string) {
+  private addDynamoDBRecord(tableName: string, tableArn: string) {
     return new DynamoDBInsertResource(this, 'dynamodb-insert', { tableName, tableArn })
   }
 
-  createLaunchTemplate(asgSg: ec2.SecurityGroup) {
+  private createLaunchTemplate(asgSg: ec2.SecurityGroup) {
     const userDataScript = readFileSync('./lib/user-data.sh', 'utf-8')
 
-    const role = new iam.Role(this, 'aws-examples-instance-role', {
+    const role = new iam.Role(this, 'instance-role', {
       assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
       managedPolicies: [iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonDynamoDBReadOnlyAccess')],
     })
 
-    return new ec2.LaunchTemplate(this, 'aws-examples-launch-template', {
+    return new ec2.LaunchTemplate(this, 'launch-template', {
       launchTemplateName: 'aws-examples-launch-template',
       securityGroup: asgSg,
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
@@ -130,7 +140,7 @@ export class Ec2Stack extends cdk.Stack {
     })
   }
 
-  createInstanceConnectEp(vpc: ec2.Vpc, asgSg: ec2.SecurityGroup) {
+  private createInstanceConnectEp(vpc: ec2.Vpc, asgSg: ec2.SecurityGroup) {
     // get the EC2_INSTANCE_CONNECT CIDR for ca-central-1
     // const cmd = `curl -s https://ip-ranges.amazonaws.com/ip-ranges.json | jq -r '.prefixes[] | select(.region=="ca-central-1") | select(.service=="EC2_INSTANCE_CONNECT") | .ip_prefix'`
     const cmd = `curl -s https://ip-ranges.amazonaws.com/ip-ranges.json`
@@ -140,7 +150,7 @@ export class Ec2Stack extends cdk.Stack {
       (p: Record<string, string>) =>
         p.service === 'EC2_INSTANCE_CONNECT' && p.region === 'ca-central-1',
     )
-    const instanceConnectSg = new ec2.SecurityGroup(this, 'aws-examples-instance-connect-sg', {
+    const instanceConnectSg = new ec2.SecurityGroup(this, 'instance-connect-sg', {
       vpc,
       securityGroupName: 'aws-examples-instance-connect-sg',
       description: 'Security group for Instance Connect',
@@ -159,18 +169,14 @@ export class Ec2Stack extends cdk.Stack {
       'Allow access from Instance Connect',
     )
 
-    const instanceConnectEndpoint = new ec2.CfnInstanceConnectEndpoint(
-      this,
-      'aws-examples-instance-connect-ep',
-      {
-        subnetId: vpc.privateSubnets[0].subnetId,
-        securityGroupIds: [instanceConnectSg.securityGroupId],
-      },
-    )
+    return new ec2.CfnInstanceConnectEndpoint(this, 'instance-connect-ep', {
+      subnetId: vpc.privateSubnets[0].subnetId,
+      securityGroupIds: [instanceConnectSg.securityGroupId],
+    })
   }
 
-  createAutoScalingGroup(vpc: ec2.Vpc, launchTemplate: ec2.LaunchTemplate) {
-    return new asg.AutoScalingGroup(this, 'aws-examples-asg', {
+  private createAutoScalingGroup(vpc: ec2.Vpc, launchTemplate: ec2.LaunchTemplate) {
+    return new asg.AutoScalingGroup(this, 'asg', {
       vpc: vpc,
       launchTemplate: launchTemplate,
       minCapacity: 1,
@@ -179,19 +185,19 @@ export class Ec2Stack extends cdk.Stack {
     })
   }
 
-  createLoadBalancer(vpc: ec2.Vpc, albSg: ec2.SecurityGroup, asg: asg.AutoScalingGroup) {
-    const lb = new elbv2.ApplicationLoadBalancer(this, 'aws-examples-alb', {
+  private createLoadBalancer(vpc: ec2.Vpc, albSg: ec2.SecurityGroup, asg: asg.AutoScalingGroup) {
+    const lb = new elbv2.ApplicationLoadBalancer(this, 'alb', {
       vpc,
       internetFacing: true,
     })
 
-    const listener = lb.addListener('aws-examples-listener', {
+    const listener = lb.addListener('listener', {
       port: 80,
     })
 
     // Create an AutoScaling group and add it as a load balancing
     // target to the listener.
-    listener.addTargets('aws-examples-asg', {
+    listener.addTargets('asg-target', {
       port: 3000,
       protocol: elbv2.ApplicationProtocol.HTTP,
       targets: [asg],
@@ -204,43 +210,52 @@ export class Ec2Stack extends cdk.Stack {
     return lb
   }
 
-  createStaticWebsite() {
+  private createStaticWebsite() {
     // create S3 bucket, upload index.html, and create CloudFront distribution
-    const s3Bucket = new s3.Bucket(this, 'aws-examples-s3-bucket', {
+    const s3Bucket = new s3.Bucket(this, 'bucket', {
       bucketName: 'aws-examples-s3-bucket',
       removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
       accessControl: s3.BucketAccessControl.PRIVATE,
     })
 
-    new BucketDeployment(this, 'aws-examples-s3-bucket-deployment', {
+    new s3Deploy.BucketDeployment(this, 'bucket-deployment', {
       destinationBucket: s3Bucket,
-      sources: [Source.asset(path.resolve(process.cwd(), '../frontend/dist'))],
+      sources: [s3Deploy.Source.asset(path.resolve(process.cwd(), '../frontend/dist'))],
     })
 
     return s3Bucket
   }
 
-  createCloudFrontDistribution(s3Bucket: s3.Bucket, lb: elbv2.ApplicationLoadBalancer) {
-    // the oac is not yet supported by CDK, the workaround taken from here: https://github.com/aws/aws-cdk/issues/21771#issuecomment-1479201394
-
-    const oac = new cloudfront.CfnOriginAccessControl(this, 'aws-examples-aoc', {
-      originAccessControlConfig: {
-        name: 'aws-examples-aoc',
-        originAccessControlOriginType: 's3',
-        signingBehavior: 'always',
-        signingProtocol: 'sigv4',
-      },
-    })
-
-    const distribution = new cloudfront.Distribution(this, 'Distribution', {
+  private createCloudFrontDistribution(s3Bucket: s3.Bucket, lb: elbv2.ApplicationLoadBalancer) {
+    const distribution = new cloudfront.Distribution(this, 'distribution', {
       comment: 'CloudFront distribution for aws-examples',
       defaultRootObject: 'index.html',
       httpVersion: cloudfront.HttpVersion.HTTP2_AND_3,
       minimumProtocolVersion: cloudfront.SecurityPolicyProtocol.TLS_V1_2016,
       defaultBehavior: {
-        origin: new s3Origins.S3Origin(s3Bucket),
+        origin: new origins.S3Origin(s3Bucket),
         allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD,
         viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        originRequestPolicy: cloudfront.OriginRequestPolicy.ALL_VIEWER_AND_CLOUDFRONT_2022,
+      },
+      additionalBehaviors: {
+        '/api/*': {
+          origin: new origins.LoadBalancerV2Origin(lb),
+          allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
+          viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+          originRequestPolicy: cloudfront.OriginRequestPolicy.ALL_VIEWER_AND_CLOUDFRONT_2022,
+        },
+      },
+    })
+
+    // the oac is not yet supported by CDK, the workaround taken from here: https://github.com/aws/aws-cdk/issues/21771#issuecomment-1479201394
+    const oac = new cloudfront.CfnOriginAccessControl(this, 'cf-oac', {
+      originAccessControlConfig: {
+        name: 'aws-examples-aoc',
+        originAccessControlOriginType: 's3',
+        signingBehavior: 'always',
+        signingProtocol: 'sigv4',
       },
     })
 
