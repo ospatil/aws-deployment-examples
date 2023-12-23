7b7b8b1d125ace3d634a7a844174390d1a4c31c2
Omkar Patil <ospatil@gmail.com>
2023-12-23 08:56:25 -0500

    Add prod mode build

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,6 +1,6 @@
 /* @refresh reload */
-import { render } from 'solid-js/web'
 import { Router } from '@solidjs/router'
+import { render } from 'solid-js/web'
 import App from './App'
 import './index.css'
 
