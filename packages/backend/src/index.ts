0dfc192df1c624d8873b0bc25d401fcc356a4f18
Omkar Patil <ospatil@gmail.com>
2023-12-28 14:21:02 -0500

    Add aws jwt header verification

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,10 +1,11 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
 import { DynamoDBDocumentClient, GetCommand } from '@aws-sdk/lib-dynamodb'
 import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { Context, Hono } from 'hono'
 import { getRuntimeKey } from 'hono/adapter'
 import { cors } from 'hono/cors'
 import { logger } from 'hono/logger'
+import * as jwt from 'jsonwebtoken'
 
 // it will use AWS_REGION set by userdata script that fetches from instance metadata
 const client = new DynamoDBClient()
@@ -21,9 +22,35 @@ app.get('/api/healthz', c => {
 app.get('/api/messages', async c => {
   const command = new GetCommand({ TableName: 'aws-examples-messages', Key: { id: 1 } })
   const response = await docClient.send(command)
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-  return c.text(response?.Item?.msg || 'Hello World!')
+
+  const user = await getUserClaims(c)
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const message: string = response?.Item?.msg || 'Hello World!'
+
+  return c.json({ message, user })
 })
+
+async function getUserClaims(c: Context) {
+  // get user claims from header. The ALB will add the header x-amzn-oidc-data after successful authentication
+  const encodedJwt = c.req.header('x-amzn-oidc-data')!
+  // decode the jwt
+  try {
+    const decodedJwt = jwt.decode(encodedJwt, { complete: true })!
+    const kid = decodedJwt.header.kid // get the public key from the jwks endpoint
+    const keyEndpoint = `https://public-keys.auth.elb.${process.env.AWS_REGION}.amazonaws.com/${kid}`
+    const response = await fetch(keyEndpoint)
+    const publicKey = await response.text()
+
+    // verify the jwt
+    const claims = jwt.verify(encodedJwt, publicKey, { algorithms: ['ES256'] })
+    return claims
+  } catch (error) {
+    console.error(error)
+  }
+
+  return {}
+}
 
 if (getRuntimeKey() === 'node') {
   serve(app)
--- packages/frontend/src/pages/Protected.tsx
+++ packages/frontend/src/pages/Protected.tsx
@@ -1,23 +1,29 @@
 import { createAsync } from '@solidjs/router'
 import { Show } from 'solid-js/web'
 
+type ReturnDataType = {
+  message: string
+  user: Record<string, unknown>
+}
+
 export const loadProtectedData = async () => {
   const apiUrl = import.meta.env.VITE_API_URI_MESSAGES as string
   const response = await fetch(apiUrl)
 
-  return response.text()
+  return response.json() as Promise<ReturnDataType>
 }
 
 export default function Protected() {
   // eslint-disable-next-line @typescript-eslint/promise-function-async
-  const message = createAsync(() => loadProtectedData())
+  const data = createAsync(() => loadProtectedData())
   return (
-    <Show when={message()}>
+    <Show when={data()}>
       <div>
         <p class="text-2xl">
-          Message from Server: <span class="bg-violet-800">{message()}</span>
+          Message from DynamoDB: <span class="bg-violet-800">{data()?.message}</span>
         </p>
       </div>
+      <pre>{JSON.stringify(data()?.user, null, 2)}</pre>
     </Show>
   )
 }
