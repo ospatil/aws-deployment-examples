a1eec9beb404ca3de9eba9ef6428c801eb758b64
Omkar Patil <ospatil@gmail.com>
2024-01-11 20:55:43 -0500

    Add lambda cdk project

---
+++ packages/lambda-apigw/test/lambda-apigw.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaApigw from '../lib/lambda-apigw-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-apigw-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaApigw.LambdaApigwStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
